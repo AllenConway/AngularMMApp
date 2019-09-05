4888bb20039d4d8d302b2d8b5c361d6187822698
Allen Conway <allenc@magenic.com>
2019-09-05 18:24:05 -0400

    Fixed MatDialog Unit Test

--- src/app/admin/admin-cabin-detail/admin-cabin-detail.component.spec.ts
+++ src/app/admin/admin-cabin-detail/admin-cabin-detail.component.spec.ts
@@ -1,23 +1,32 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AdminCabinDetailComponent } from './admin-cabin-detail.component';
-import { MatFormFieldModule, MatDialogModule, MatTableModule, MatDialogRef } from '@angular/material';
+import { MatFormFieldModule, MatDialogModule, MatTableModule, MatDialogRef, MAT_DIALOG_DATA, MatInputModule } from '@angular/material';
 import { FormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('AdminCabinDetailComponent', () => {
   let component: AdminCabinDetailComponent;
   let fixture: ComponentFixture<AdminCabinDetailComponent>;
+
+  const mockDialogRef = {
+    close: jasmine.createSpy('close')
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
         FormsModule,
         MatDialogModule,
-        MatDialogRef,
         MatFormFieldModule,
-        MatTableModule
+        MatInputModule,
+        NoopAnimationsModule
       ],
-      declarations: [ AdminCabinDetailComponent ]
+      declarations: [ AdminCabinDetailComponent ],
+      providers: [
+        {provide: MatDialogRef, useValue: mockDialogRef},
+        {provide: MAT_DIALOG_DATA, useValue: {} },
+      ]
     })
     .compileComponents();
   }));
@@ -28,7 +37,7 @@ describe('AdminCabinDetailComponent', () => {
     fixture.detectChanges();
   });
 
-  xit('should create', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
