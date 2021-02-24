401fb4a7ebb6001cca397706690a1f02f4dd6cd2
Allen Conway <allenc@magenic.com>
2021-02-23 23:05:20 -0500

    Added strongly typed image model

--- src/app/cabins/models/cabin.ts
+++ src/app/cabins/models/cabin.ts
@@ -1,8 +1,9 @@
+import { Images } from './images';
 export interface Cabin {
     id: number;
     name: string;
     location: string;
     occupancy: number;
     mainImageUrl?: string;
-    images?: string[];
+    images?: Images[];
   }
