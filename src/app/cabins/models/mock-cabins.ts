45341a5a60e7991bc9b09718e5222385f99b913b
Allen Conway <allenc@magenic.com>
2021-02-24 15:33:02 -0500

    Updated cabins mock data to include images

--- src/app/cabins/models/mock-cabins.ts
+++ src/app/cabins/models/mock-cabins.ts
@@ -1,25 +1,43 @@
 import { Cabin } from './';
 
 export const CABINS: Cabin[] = [
-    {
-      id: 1,
-      name: 'Mountain View',
-      location: 'Smoky Mountain West',
-      occupancy: 4,
-      mainImageUrl: '../../assets/images/Cabin1.jpg'
-    },
-    {
-      id: 2,
-      name: 'Hillside Escape',
-      location: 'Smoky Mountain North',
-      occupancy: 6,
-      mainImageUrl: '../../assets/images/Cabin3.jpg'
-    },
-    {
-      id: 3,
-      name: 'Bear Tracks',
-      location: 'Smoky Mountain South',
-      occupancy: 12,
-      mainImageUrl: '../../assets/images/Cabin3.jpg'
-    }
+  {
+    "id": 1,
+    "name": "Mountain View",
+    "location": "Smoky Mountain West",
+    "occupancy": 4,
+    "images": [
+      {"url": "../../assets/images/Cabin1.jpg"},
+      {"url": "../../assets/images/Cabin2.jpg"},
+      {"url": "../../assets/images/Cabin3.jpg"},
+      {"url": "../../assets/images/Cabin1.jpg"},
+      {"url": "../../assets/images/Cabin2.jpg"},
+      {"url": "../../assets/images/Cabin3.jpg"}
+    ],
+    "mainImageUrl": "../../assets/images/Cabin1.jpg"
+  },
+  {
+    "id": 2,
+    "name": "Hillside Escape",
+    "location": "Smoky Mountain North",
+    "occupancy": 6,
+    "images": [
+      {"url": "../../assets/images/Cabin1.jpg"},
+      {"url": "../../assets/images/Cabin2.jpg"},
+      {"url": "../../assets/images/Cabin3.jpg"}
+    ],
+    "mainImageUrl": "../../assets/images/Cabin2.jpg"
+  },
+  {
+    "id": 3,
+    "name": "Bear Tracks",
+    "location": "Smoky Mountain South",
+    "occupancy": 12,
+    "images": [
+      {"url": "../../assets/images/Cabin1.jpg"},
+      {"url": "../../assets/images/Cabin2.jpg"},
+      {"url": "../../assets/images/Cabin3.jpg"}
+    ],
+    "mainImageUrl": "../../assets/images/Cabin3.jpg"
+  }
 ];
